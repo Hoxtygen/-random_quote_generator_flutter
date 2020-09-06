52ae382050dc980dd888e772571b510521c55e0f
Wasiu Idowu <hoxtygen@live.com>
2020-09-06 09:13:55 +0100

    Add single quote class and quotes

---
+++ lib/quoteBrain.dart
@@ -0,0 +1,39 @@
+import 'quote.dart';
+
+class QuoteBrain {
+  int _quoteNumber = 0;
+
+  List<Quote> _quoteData = [
+    Quote("No one can make you feel inferior without your consent.",
+        "Eleanor Roosevelt,"),
+    Quote(
+        "It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it.",
+        "Maurice Switzer"),
+    Quote(
+        "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+        " William Shakespeare"),
+    Quote(
+        "Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).",
+        " Mark Twain "),
+    Quote("The only true wisdom is in knowing you know nothing.", " Socrates"),
+    Quote(
+        "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+        " Isaac Asimov"),
+    Quote(
+        "Hold fast to dreams,For if dreams dieLife is a broken-winged bird,That cannot fly.",
+        "Langston Hughes"),
+    Quote(
+        "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+        " Aristotle"),
+    Quote("Any fool can know. The point is to understand.", "Albert Einstein"),
+    Quote(
+        "The best index to a person's character is how he treats people who can't do him any good, and how he treats people who can't fight back.",
+        " Abigail Van Buren"),
+    Quote(
+        "There are three things all wise men fear: the sea in storm, a night with no moon, and the anger of a gentle man.",
+        " Patrick Rothfuss"),
+    Quote(
+        "By three methods we may learn wisdom: First, by reflection, which is noblest; Second, by imitation, which is easiest; and third by experience, which is the bitterest.",
+        "Confucius"),
+  ];
+}
